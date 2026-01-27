dc0cf961a26622ff2b84286ce2831cef67349574
Historico-MSS <jcpendienteswork@gmail.com>
2026-01-27 18:28:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title="Generador de Ilustraciones", page_icon="📈")
 # --- 🔐 SISTEMA DE CONTRASEÑA ---
 def check_password():
     def password_entered():
-        if st.session_state["password"] == "dominion2025": 
+        if st.session_state["password"] == "historico": 
             st.session_state["password_correct"] = True
             del st.session_state["password"]
         else:
